a8df189e13e35f299498609ef6a39c00a072c2dd
Randon Spackman <randon.spackman@gmail.com>
2018-06-18 10:40:58 -0600

    Use objects

--- Source/Pattern.cpp
+++ Source/Pattern.cpp
@@ -16,171 +16,139 @@ namespace Shelfinator
 	Pattern::Pattern(FILE *file)
 	{
 		ReadLights(file);
-		ReadLightSequences(file);
+		lightSequences.Read(file, length);
 		ReadColors(file);
-		ReadPaletteSequences(file);
-	}
-
-	Pattern::~Pattern()
-	{
-		FreeLights();
-		FreeLightSequences();
-		FreeColors();
-		FreePaletteSequences();
+		paletteSequences.Read(file);
 	}
 
 	void Pattern::ReadLights(FILE *file)
 	{
-		numLights = ReadInt(file);
-		lightStartTime = new int*[numLights];
-		lightEndTime = new int*[numLights];
-		lightOrigEndTime = new int*[numLights];
-		lightStartColorIndex = new int*[numLights];
-		lightStartColorValue = new int*[numLights];
-		lightEndColorIndex = new int*[numLights];
-		lightEndColorValue = new int*[numLights];
-		lightIntermediates = new int*[numLights];
-		currentIndex = new int[numLights];
-
-		for (auto lightCtr = 0; lightCtr < numLights; ++lightCtr)
-		{
-			auto dataCount = ReadInt(file);
-			lightStartTime[lightCtr] = new int[dataCount];
-			lightEndTime[lightCtr] = new int[dataCount];
-			lightOrigEndTime[lightCtr] = new int[dataCount];
-			lightStartColorIndex[lightCtr] = new int[dataCount];
-			lightStartColorValue[lightCtr] = new int[dataCount];
-			lightEndColorIndex[lightCtr] = new int[dataCount];
-			lightEndColorValue[lightCtr] = new int[dataCount];
-			lightIntermediates[lightCtr] = new int[dataCount];
-			currentIndex[lightCtr] = 0;
-
-			for (auto dataCtr = 0; dataCtr < dataCount; ++dataCtr)
-			{
-				lightStartTime[lightCtr][dataCtr] = ReadInt(file);
-				lightEndTime[lightCtr][dataCtr] = ReadInt(file);
-				lightOrigEndTime[lightCtr][dataCtr] = ReadInt(file);
-				lightStartColorIndex[lightCtr][dataCtr] = ReadInt(file);
-				lightStartColorValue[lightCtr][dataCtr] = ReadInt(file);
-				lightEndColorIndex[lightCtr][dataCtr] = ReadInt(file);
-				lightEndColorValue[lightCtr][dataCtr] = ReadInt(file);
-				lightIntermediates[lightCtr][dataCtr] = ReadBool(file);
-			}
-		}
+		lightData.resize(ReadInt(file));
+		for (auto ctr = 0; ctr < lightData.size(); ++ctr)
+			lightData[ctr].Read(file);
 	}
 
-	void Pattern::FreeLights()
+	void Pattern::LightItem::Read(FILE *file)
 	{
-		for (auto ctr = 0; ctr < numLights; ++ctr)
-		{
-			delete[] lightStartTime[ctr];
-			delete[] lightEndTime[ctr];
-			delete[] lightOrigEndTime[ctr];
-			delete[] lightStartColorIndex[ctr];
-			delete[] lightStartColorValue[ctr];
-			delete[] lightEndColorIndex[ctr];
-			delete[] lightEndColorValue[ctr];
-			delete[] lightIntermediates[ctr];
-		}
-		delete[] lightStartTime;
-		delete[] lightEndTime;
-		delete[] lightOrigEndTime;
-		delete[] lightStartColorIndex;
-		delete[] lightStartColorValue;
-		delete[] lightEndColorIndex;
-		delete[] lightEndColorValue;
-		delete[] lightIntermediates;
-		delete[] currentIndex;
+		startTime = ReadInt(file);
+		endTime = ReadInt(file);
+		origEndTime = ReadInt(file);
+		startColorIndex = ReadInt(file);
+		startColorValue = ReadInt(file);
+		endColorIndex = ReadInt(file);
+		endColorValue = ReadInt(file);
+		intermediates = ReadBool(file);
 	}
 
-	void Pattern::ReadLightSequences(FILE *file)
+	double Pattern::LightItem::GetPercent(double time)
 	{
-		auto numLightSequences = ReadInt(file);
-		lightSequenceLightStartTime = new int[numLightSequences];
-		lightSequenceLightEndTime = new int[numLightSequences];
-		lightSequenceStartTime = new int[numLightSequences];
-		lightSequenceEndTime = new int[numLightSequences];
+		return (time - startTime) / (origEndTime - startTime);
+	}
+
+	double Pattern::LightItem::GetSameIndexColorValue(double time)
+	{
+		return (time - startTime) * (endColorValue - startColorValue) / (origEndTime - startTime) + startColorValue;
+	}
+
+	void Pattern::Light::Read(FILE *file)
+	{
+		lights.resize(ReadInt(file));
+		for (auto ctr = 0; ctr < lights.size(); ++ctr)
+			lights[ctr].Read(file);
+	}
+
+	Pattern::LightItem &Pattern::Light::LightAtTime(double lightTime)
+	{
+		while (lightTime < lights[current].startTime)
+			--current;
+		while (lightTime >= lights[current].endTime)
+			++current;
+		return lights[current];
+	}
+
+	void Pattern::LightSequence::Read(FILE *file, int &length)
+	{
+		lightStartTime = ReadInt(file);
+		lightEndTime = ReadInt(file);
+		startTime = length;
+		endTime = length = length + ReadInt(file);
+	}
+
+	double Pattern::LightSequence::GetLightTime(int time)
+	{
+		return ((double)time - startTime) / (endTime - startTime) * (lightEndTime - lightStartTime) + lightStartTime;
+	}
+
+	void Pattern::LightSequences::Read(FILE *file, int &length)
+	{
+		lightSequences.resize(ReadInt(file));
 		length = 0;
-		for (auto lightSequenceCtr = 0; lightSequenceCtr < numLightSequences; ++lightSequenceCtr)
+		for (auto ctr = 0; ctr < lightSequences.size(); ++ctr)
+			lightSequences[ctr].Read(file, length);
+	}
+
+	Pattern::LightSequence & Pattern::LightSequences::SequenceAtTime(int time)
+	{
+		while (time < lightSequences[current].startTime)
+			--current;
+		while (time >= lightSequences[current].endTime)
+			++current;
+		return lightSequences[current];
+	}
+
+	void Pattern::LightColor::Read(FILE *file)
+	{
+		minValue = ReadInt(file);
+		maxValue = ReadInt(file);
+		colors.resize(ReadInt(file));
+		for (auto paletteCtr = 0; paletteCtr < colors.size(); ++paletteCtr)
 		{
-			lightSequenceLightStartTime[lightSequenceCtr] = ReadInt(file);
-			lightSequenceLightEndTime[lightSequenceCtr] = ReadInt(file);
-			lightSequenceStartTime[lightSequenceCtr] = length;
-			lightSequenceEndTime[lightSequenceCtr] = length = length + ReadInt(file);
+			colors[paletteCtr].resize(ReadInt(file));
+			for (auto colorCtr = 0; colorCtr < colors[paletteCtr].size(); ++colorCtr)
+				colors[paletteCtr][colorCtr] = ReadInt(file);
 		}
 	}
 
-	void Pattern::FreeLightSequences()
+	void Pattern::LightColor::GradientColor(double value, int index, double &red, double &green, double &blue)
 	{
-		delete[] lightSequenceLightStartTime;
-		delete[] lightSequenceLightEndTime;
-		delete[] lightSequenceStartTime;
-		delete[] lightSequenceEndTime;
+		index = index % colors.size();
+		Helpers::GradientColor(value, minValue, maxValue, colors[index].data(), (int)colors[index].size(), red, green, blue);
 	}
 
 	void Pattern::ReadColors(FILE *file)
 	{
-		numColors = ReadInt(file);
-		colorMinValue = new int[numColors];
-		colorMaxValue = new int[numColors];
-		colorCount = new int[numColors];
-		colorColorCount = new int*[numColors];
-		colorColors = new int**[numColors];
-		for (auto colorCtr = 0; colorCtr < numColors; ++colorCtr)
-		{
-			colorMinValue[colorCtr] = ReadInt(file);
-			colorMaxValue[colorCtr] = ReadInt(file);
-			colorCount[colorCtr] = ReadInt(file);
-			colorColorCount[colorCtr] = new int[colorCount[colorCtr]];
-			colorColors[colorCtr] = new int*[colorCount[colorCtr]];
-			for (auto colorIndexCtr = 0; colorIndexCtr < colorCount[colorCtr]; ++colorIndexCtr)
-			{
-				colorColorCount[colorCtr][colorIndexCtr] = ReadInt(file);
-				colorColors[colorCtr][colorIndexCtr] = new int[colorColorCount[colorCtr][colorIndexCtr]];
-				for (auto colorColorCountCtr = 0; colorColorCountCtr < colorColorCount[colorCtr][colorIndexCtr]; ++colorColorCountCtr)
-					colorColors[colorCtr][colorIndexCtr][colorColorCountCtr] = ReadInt(file);
-			}
-		}
+		lightColors.resize(ReadInt(file));
+		for (auto ctr = 0; ctr < lightColors.size(); ++ctr)
+			lightColors[ctr].Read(file);
 	}
 
-	void Pattern::FreeColors()
+	void Pattern::PaletteSequences::Read(FILE *file)
 	{
-		for (auto colorCtr = 0; colorCtr < numColors; ++colorCtr)
-		{
-			for (auto ctr = 0; ctr < colorCount[colorCtr]; ++ctr)
-				delete[] colorColors[colorCtr][ctr];
-			delete[] colorColors[colorCtr];
-			delete[] colorColorCount[colorCtr];
-		}
-		delete[] colorMinValue;
-		delete[] colorMaxValue;
-		delete[] colorCount;
-		delete[] colorColorCount;
-		delete[] colorColors;
+		paletteSequences.resize(ReadInt(file));
+		for (auto ctr = 0; ctr < paletteSequences.size(); ++ctr)
+			paletteSequences[ctr].Read(file);
 	}
 
-	void Pattern::ReadPaletteSequences(FILE *file)
+	void Pattern::PaletteSequence::Read(FILE *file)
 	{
-		auto numPaletteSequences = ReadInt(file);
-		paletteSequenceStartTime = new int[numPaletteSequences];
-		paletteSequenceEndTime = new int[numPaletteSequences];
-		paletteSequenceStartColorIndex = new int[numPaletteSequences];
-		paletteSequenceEndColorIndex = new int[numPaletteSequences];
-		for (auto paletteSequenceCtr = 0; paletteSequenceCtr < numPaletteSequences; ++paletteSequenceCtr)
-		{
-			paletteSequenceStartTime[paletteSequenceCtr] = ReadInt(file);
-			paletteSequenceEndTime[paletteSequenceCtr] = ReadInt(file);
-			paletteSequenceStartColorIndex[paletteSequenceCtr] = ReadInt(file);
-			paletteSequenceEndColorIndex[paletteSequenceCtr] = ReadInt(file);
-		}
+		startTime = ReadInt(file);
+		endTime = ReadInt(file);
+		startPaletteIndex = ReadInt(file);
+		endPaletteIndex = ReadInt(file);
 	}
 
-	void Pattern::FreePaletteSequences()
+	double Pattern::PaletteSequence::GetPercent(int time)
 	{
-		delete[] paletteSequenceStartTime;
-		delete[] paletteSequenceEndTime;
-		delete[] paletteSequenceStartColorIndex;
-		delete[] paletteSequenceEndColorIndex;
+		return startPaletteIndex == endPaletteIndex ? 0 : ((double)time - startTime) / (endTime - startTime);
+	}
+
+	Pattern::PaletteSequence &Pattern::PaletteSequences::SequenceAtTime(int time)
+	{
+		while (time < paletteSequences[current].startTime)
+			--current;
+		while (time >= paletteSequences[current].endTime)
+			++current;
+		return paletteSequences[current];
 	}
 
 	void Pattern::SetLights(int time, Lights::ptr lights)
@@ -188,81 +156,45 @@ namespace Shelfinator
 		if (time < 0)
 			return;
 
-		while (time < paletteSequenceStartTime[currentPaletteSequence])
-			--currentPaletteSequence;
-		while (time >= paletteSequenceEndTime[currentPaletteSequence])
-			++currentPaletteSequence;
+		auto paletteSequence = paletteSequences.SequenceAtTime(time);
+		auto palettePercent = paletteSequence.GetPercent(time);
 
-		auto startPaletteIndex = paletteSequenceStartColorIndex[currentPaletteSequence];
-		auto endPaletteIndex = paletteSequenceEndColorIndex[currentPaletteSequence];
-		auto palettePercent = startPaletteIndex == endPaletteIndex ? 0 : ((double)time - paletteSequenceStartTime[currentPaletteSequence]) / (paletteSequenceEndTime[currentPaletteSequence] - paletteSequenceStartTime[currentPaletteSequence]);
+		auto lightTime = lightSequences.SequenceAtTime(time).GetLightTime(time);
 
-		while (time < lightSequenceStartTime[currentLightSequence])
-			--currentLightSequence;
-		while (time >= lightSequenceEndTime[currentLightSequence])
-			++currentLightSequence;
-
-		auto lightTime = ((double)time - lightSequenceStartTime[currentLightSequence]) / (lightSequenceEndTime[currentLightSequence] - lightSequenceStartTime[currentLightSequence]) * (lightSequenceLightEndTime[currentLightSequence] - lightSequenceLightStartTime[currentLightSequence]) + lightSequenceLightStartTime[currentLightSequence];
-
-		for (auto light = 0; light < numLights; ++light)
+		for (auto lightCtr = 0; lightCtr < lightData.size(); ++lightCtr)
 		{
-			while (lightTime < lightStartTime[light][currentIndex[light]])
-				--currentIndex[light];
-			while (lightTime >= lightEndTime[light][currentIndex[light]])
-				++currentIndex[light];
+			auto light = lightData[lightCtr].LightAtTime(lightTime);
 
-			auto startColorTime = lightStartTime[light][currentIndex[light]];
-			auto startColorIndex = lightStartColorIndex[light][currentIndex[light]];
-			auto startColorValue = lightStartColorValue[light][currentIndex[light]];
-			auto startColorMinValue = colorMinValue[startColorIndex];
-			auto startColorMaxValue = colorMaxValue[startColorIndex];
-			auto startColorColors = colorColors[startColorIndex];
-			auto startColorColorCount = colorColorCount[startColorIndex];
-
-			auto endColorTime = lightOrigEndTime[light][currentIndex[light]];
-			auto endColorIndex = lightEndColorIndex[light][currentIndex[light]];
-			auto endColorValue = lightEndColorValue[light][currentIndex[light]];
-			auto endColorMinValue = colorMinValue[endColorIndex];
-			auto endColorMaxValue = colorMaxValue[endColorIndex];
-			auto endColorColors = colorColors[endColorIndex];
-			auto endColorColorCount = colorColorCount[endColorIndex];
-
-			auto intermediates = lightIntermediates[light][currentIndex[light]];
-
-			auto colorPercent = (lightTime - startColorTime) / (endColorTime - startColorTime);
-			auto sameIndexColorValue = (lightTime - startColorTime) * (endColorValue - startColorValue) / (endColorTime - startColorTime) + startColorValue;
-
-			auto useSpscIndex = startPaletteIndex % colorCount[startColorIndex];
-			auto useSpecIndex = startPaletteIndex % colorCount[endColorIndex];
-			auto useEpscIndex = endPaletteIndex % colorCount[startColorIndex];
-			auto useEpecIndex = endPaletteIndex % colorCount[endColorIndex];
+			auto sameIndexColorValue = light.GetSameIndexColorValue(lightTime);
 
 			double scr, scg, scb;
-			if (startColorIndex == -1)
-				Helpers::SplitColor(startColorValue, scr, scg, scb);
+			if (light.startColorIndex == -1)
+				Helpers::SplitColor(light.startColorValue, scr, scg, scb);
 			else
 			{
 				double spscr, spscg, spscb, epscr, epscg, epscb;
-				Helpers::GradientColor(intermediates ? sameIndexColorValue : startColorValue, startColorMinValue, startColorMaxValue, startColorColors[useSpscIndex], startColorColorCount[useSpscIndex], spscr, spscg, spscb);
-				Helpers::GradientColor(intermediates ? sameIndexColorValue : startColorValue, startColorMinValue, startColorMaxValue, startColorColors[useEpscIndex], startColorColorCount[useEpscIndex], epscr, epscg, epscb);
+				auto value = light.intermediates ? sameIndexColorValue : light.startColorValue;
+				lightColors[light.startColorIndex].GradientColor(value, paletteSequence.startPaletteIndex, spscr, spscg, spscb);
+				lightColors[light.startColorIndex].GradientColor(value, paletteSequence.endPaletteIndex, epscr, epscg, epscb);
 				Helpers::GradientColor(spscr, spscg, spscb, epscr, epscg, epscb, palettePercent, scr, scg, scb);
 			}
 
 			double ecr, ecg, ecb;
-			if (endColorIndex == -1)
-				Helpers::SplitColor(endColorValue, ecr, ecg, ecb);
+			if (light.endColorIndex == -1)
+				Helpers::SplitColor(light.endColorValue, ecr, ecg, ecb);
 			else
 			{
 				double specr, specg, specb, epecr, epecg, epecb;
-				Helpers::GradientColor(intermediates ? sameIndexColorValue : endColorValue, endColorMinValue, endColorMaxValue, endColorColors[useSpecIndex], endColorColorCount[useSpecIndex], specr, specg, specb);
-				Helpers::GradientColor(intermediates ? sameIndexColorValue : endColorValue, endColorMinValue, endColorMaxValue, endColorColors[useEpecIndex], endColorColorCount[useEpecIndex], epecr, epecg, epecb);
+				auto value = light.intermediates ? sameIndexColorValue : light.endColorValue;
+				lightColors[light.endColorIndex].GradientColor(value, paletteSequence.startPaletteIndex, specr, specg, specb);
+				lightColors[light.endColorIndex].GradientColor(value, paletteSequence.endPaletteIndex, epecr, epecg, epecb);
 				Helpers::GradientColor(specr, specg, specb, epecr, epecg, epecb, palettePercent, ecr, ecg, ecb);
 			}
 
 			double r, g, b;
-			Helpers::GradientColor(scr, scg, scb, ecr, ecg, ecb, colorPercent, r, g, b);
+			Helpers::GradientColor(scr, scg, scb, ecr, ecg, ecb, light.GetPercent(lightTime), r, g, b);
 
-			lights->SetLight(light, Helpers::CombineColor(r, g, b));
+			lights->SetLight(lightCtr, Helpers::CombineColor(r, g, b));
 		}
 	}
 
--- Source/Pattern.h
+++ Source/Pattern.h
@@ -10,31 +10,77 @@ namespace Shelfinator
 		typedef std::shared_ptr<Pattern> ptr;
 
 		static ptr Read(const char *fileName);
-		~Pattern();
 
 		void SetLights(int time, Lights::ptr lights);
 		int GetLength();
 	private:
-		int numLights;
-		int **lightStartTime, **lightEndTime, **lightOrigEndTime, **lightStartColorIndex, **lightStartColorValue, **lightEndColorIndex, **lightEndColorValue, **lightIntermediates;
-		int *currentIndex;
+		class LightItem
+		{
+		public:
+			int startTime, endTime, origEndTime, startColorIndex, startColorValue, endColorIndex, endColorValue, intermediates;
+			void Read(FILE *file);
+			double GetPercent(double time);
+			double GetSameIndexColorValue(double time);
+		};
+		class Light
+		{
+		public:
+			void Read(FILE *file);
+			LightItem &LightAtTime(double time);
+		private:
+			std::vector<LightItem> lights;
+			int current = 0;
+		};
+		std::vector<Light> lightData;
 		void ReadLights(FILE *file);
-		void FreeLights();
 
-		int length = 0, currentLightSequence = 0;
-		int *lightSequenceLightStartTime, *lightSequenceLightEndTime, *lightSequenceStartTime, *lightSequenceEndTime;
-		void ReadLightSequences(FILE *file);
-		void FreeLightSequences();
+		class LightSequence
+		{
+		public:
+			int lightStartTime, lightEndTime, startTime, endTime;
+			void Read(FILE *file, int &length);
+			double GetLightTime(int time);
+		};
+		class LightSequences
+		{
+		public:
+			void Read(FILE *file, int &length);
+			LightSequence & SequenceAtTime(int time);
+		private:
+			std::vector<LightSequence> lightSequences;
+			int current = 0;
+		};
+		int length = 0;
+		LightSequences lightSequences;
 
-		int numColors;
-		int *colorMinValue, *colorMaxValue, *colorCount, **colorColorCount, ***colorColors;
+		class LightColor
+		{
+		public:
+			int minValue, maxValue;
+			std::vector<std::vector<int>> colors;
+			void Read(FILE *file);
+			void GradientColor(double value, int index, double &red, double &green, double &blue);
+		};
+		std::vector<LightColor> lightColors;
 		void ReadColors(FILE *file);
-		void FreeColors();
 
-		int currentPaletteSequence = 0;
-		int *paletteSequenceStartTime, *paletteSequenceEndTime, *paletteSequenceStartColorIndex, *paletteSequenceEndColorIndex;
-		void ReadPaletteSequences(FILE *file);
-		void FreePaletteSequences();
+		class PaletteSequence
+		{
+		public:
+			int startTime, endTime, startPaletteIndex, endPaletteIndex;
+			void Read(FILE *file);
+			double GetPercent(int time);
+		};
+		class PaletteSequences
+		{
+		public:
+			void Read(FILE *file);
+			PaletteSequence &SequenceAtTime(int time);
+		private:
+			std::vector<PaletteSequence> paletteSequences;
+			int current = 0;
+		};
+		PaletteSequences paletteSequences;
 
 		Pattern(FILE *file);
 		static bool ReadBool(FILE *file);
