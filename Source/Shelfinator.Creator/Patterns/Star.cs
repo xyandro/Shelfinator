df36c3c53c94bc10b3d2e400608f84e7ba8efa74
Randon Spackman <randon.spackman@gmail.com>
2018-09-14 16:27:23 -0600

    Add circle to star

--- Source/Shelfinator.Creator/Patterns/Star.cs
+++ Source/Shelfinator.Creator/Patterns/Star.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -20,51 +21,37 @@ namespace Shelfinator.Creator.Patterns
 
 			var pattern = new Pattern();
 
-			var useColor = new Dictionary<int, int>
-			{
-				[1] = 0xff0000,
-				[2] = 0xff0000,
-				[3] = 0xff0000,
-				[4] = 0x00ff00,
-				[5] = 0x0000ff,
-			};
+			var useColor = new List<int> { 0xff0000, 0xff0000, 0xff0000, 0x00ff00, 0x0000ff, 0xff00ff };
 			var time = 0;
-			for (var numPoints = 2; numPoints <= 5; ++numPoints)
+			var value = -1;
+			while (true)
 			{
+				++value;
 				var startTime = time;
 				for (var angle = 0; angle < 360; angle += 5)
 				{
-					var percent = angle / 360D;
-					AddPoints(pattern, layout, numPoints, angle, percent, time, Brightness, Helpers.GradientColor(useColor[numPoints - 1], useColor[numPoints], percent));
+					Render(pattern, layout, value, angle, angle / 360D, time, Brightness, Helpers.GradientColor(useColor[value], useColor[value + 1], angle / 360D));
 					time += 5;
 				}
 				pattern.AddLightSequence(startTime, time, 2500, 1);
+				if (value == 4)
+					break;
 
 				startTime = time;
 				for (var angle = 0; angle < 360; angle += 5)
 				{
-					AddPoints(pattern, layout, numPoints, angle, 1, time, Brightness, useColor[numPoints]);
+					Render(pattern, layout, value, angle, 1, time, Brightness, useColor[value + 1]);
 					time += 5;
 				}
 				pattern.AddLightSequence(startTime, time, 2500, 2);
 			}
 
-
 			return pattern;
 		}
 
-		private void AddPoints(Pattern pattern, Layout layout, int numPoints, double addAngle, double firstPercent, int time, double Brightness, int color)
+		void Render(Pattern pattern, Layout layout, int currentValue, double addAngle, double firstPercent, int time, double brightness, int color)
 		{
-			var points = GetPoints(numPoints, addAngle, firstPercent);
-
-			var canvas = new Canvas() { Width = 97, Height = 97 };
-			var bytes = BitConverter.GetBytes(color);
-			var brush = new SolidColorBrush(Color.FromArgb(255, bytes[2], bytes[1], bytes[0]));
-			var pg = new Polygon { Stroke = Brushes.White, Fill = brush };
-			foreach (var point in points)
-				pg.Points.Add(point);
-			canvas.Children.Add(pg);
-
+			var canvas = GetCanvas(currentValue, addAngle, firstPercent, color);
 			canvas.Measure(new Size((int)canvas.Width, (int)canvas.Height));
 			canvas.Arrange(new Rect(new Size((int)canvas.Width, (int)canvas.Height)));
 
@@ -79,30 +66,82 @@ namespace Shelfinator.Creator.Patterns
 				{
 					var value = (int)(buffer[bufferPos++] & 0xffffff);
 					foreach (var light in layout.GetPositionLights(x, y, 1, 1))
-						pattern.AddLight(light, time, pattern.Absolute, Helpers.MultiplyColor(value, Brightness));
+						pattern.AddLight(light, time, pattern.Absolute, Helpers.MultiplyColor(value, brightness));
 				}
 		}
 
-		List<Point> GetPoints(int numPoints, double addAngle, double firstPercent)
+		Canvas GetCanvas(int currentValue, double addAngle, double firstPercent, int color)
 		{
-			var points = new List<Point>();
-			var dist = 48D;
-			if (numPoints == 2)
+			var canvas = new Canvas() { Width = 97, Height = 97 };
+			var pathFigure = new PathFigure { IsClosed = true };
+
+			var bytes = BitConverter.GetBytes(color);
+			var brush = new SolidColorBrush(Color.FromArgb(255, bytes[2], bytes[1], bytes[0]));
+
+			if (currentValue < 3)
 			{
-				dist *= firstPercent;
-				firstPercent = 1;
+				var numPoints = currentValue + 2;
+				var dist = 48D;
+				switch (currentValue)
+				{
+					case 0:
+						dist *= firstPercent;
+						firstPercent = 1;
+						break;
+					case 3:
+						firstPercent = 10 * firstPercent + 5;
+						numPoints = (int)firstPercent;
+						firstPercent -= numPoints;
+						break;
+				}
+
+				for (var ctr = 0; ctr < numPoints; ++ctr)
+				{
+					var prevAngle = 360 / (numPoints - 1) * ctr;
+					var curAngle = 360 / numPoints * ctr;
+					var angle = (prevAngle + (curAngle - prevAngle) * firstPercent + addAngle) * Math.PI / 180;
+					var point = new Point(48 + Math.Sin(angle) * dist, 48 - Math.Cos(angle) * dist);
+					if (ctr == 0)
+						pathFigure.StartPoint = point;
+					else
+						pathFigure.Segments.Add(new LineSegment(point, true));
+				}
 			}
-			for (var ctr = 0; ctr < numPoints; ++ctr)
+			else
 			{
-				var prevAngle = 360 / (numPoints - 1) * ctr;
-				var curAngle = 360 / numPoints * ctr;
-				var angle = (prevAngle + (curAngle - prevAngle) * firstPercent + addAngle) * Math.PI / 180;
-				var x = 48 + Math.Sin(angle) * dist;
-				var y = 48 - Math.Cos(angle) * dist;
-				points.Add(new Point(x, y));
+				if ((currentValue == 4) || (firstPercent >= 1))
+					addAngle = 0;
+
+				var distancePercent = currentValue == 3 ? 1D : 1 - firstPercent;
+
+				var points1 = new List<Point> { new Point(48, 96), new Point(72, 72), new Point(72, 72), new Point(96, 48), new Point(72, 24), new Point(72, 24), new Point(48, 0), new Point(24, 24), new Point(24, 24), new Point(0, 48), new Point(24, 72), new Point(24, 72) };
+				var points2 = new List<Point> { new Point(48, 86.4), new Point(69.12, 86.4), new Point(86.4, 69.12), new Point(86.4, 48), new Point(86.4, 26.88), new Point(69.12, 9.6), new Point(48, 9.6), new Point(26.88, 9.6), new Point(9.6, 26.88), new Point(9.6, 48), new Point(9.6, 69.12), new Point(26.88, 86.4) };
+
+				var center = new Point(48, 48);
+
+				var angles = points1.Select(p => p - center).Select(p => Math.Atan2(p.X, p.Y) + addAngle * Math.PI / 180).ToList();
+				var distance = points1.Select(p => p - center).Select(p => p.Length * distancePercent).ToList();
+				points1 = points1.Select((p, indexVal) => new Point(center.X + distance[indexVal] * Math.Sin(angles[indexVal]), center.Y - distance[indexVal] * Math.Cos(angles[indexVal]))).ToList();
+
+				angles = points2.Select(p => p - center).Select(p => Math.Atan2(p.X, p.Y) + addAngle * Math.PI / 180).ToList();
+				distance = points2.Select(p => p - center).Select(p => p.Length * distancePercent).ToList();
+				points2 = points2.Select((p, indexVal) => new Point(center.X + distance[indexVal] * Math.Sin(angles[indexVal]), center.Y - distance[indexVal] * Math.Cos(angles[indexVal]))).ToList();
+
+				List<Point> usePoints;
+				if (currentValue == 3)
+					usePoints = Enumerable.Range(0, points1.Count).Select(x => new Point(points1[x].X + (points2[x].X - points1[x].X) * firstPercent, points1[x].Y + (points2[x].Y - points1[x].Y) * firstPercent)).ToList();
+				else
+					usePoints = points2;
+
+				pathFigure.StartPoint = usePoints[0];
+				pathFigure.Segments.Add(new BezierSegment(usePoints[1], usePoints[2], usePoints[3], true));
+				pathFigure.Segments.Add(new BezierSegment(usePoints[4], usePoints[5], usePoints[6], true));
+				pathFigure.Segments.Add(new BezierSegment(usePoints[7], usePoints[8], usePoints[9], true));
+				pathFigure.Segments.Add(new BezierSegment(usePoints[10], usePoints[11], usePoints[0], true));
 			}
 
-			return points;
+			canvas.Children.Add(new Path { Stroke = Brushes.White, Fill = brush, Data = new PathGeometry { Figures = new PathFigureCollection { pathFigure } } });
+			return canvas;
 		}
 	}
 }
