561de6ce8295a8bdc3f97e852d1070e3490ea10c
rs <spamnuker2000@yahoo.com>
2018-09-22 02:03:53 -0600

    Update Tag.cs

--- Source/Shelfinator.Creator/Patterns/Tag.cs
+++ Source/Shelfinator.Creator/Patterns/Tag.cs
@@ -8,8 +8,6 @@ namespace Shelfinator.Creator.Patterns
 	class Tag : IPattern
 	{
 		public int PatternNumber => 39;
-		const int HeaderOffsetX = 32;
-		const int HeaderOffsetY = -8;
 		const int RunAwayDistance = 20;
 		static Random rand = new Random(0xdec0de);
 		Layout headerLayout, bodyLayout;
@@ -18,18 +16,12 @@ namespace Shelfinator.Creator.Patterns
 		{
 			public Player()
 			{
-				position = new Point(rand.Next(32) + HeaderOffsetX, rand.Next(8) + HeaderOffsetY);
+				Position = new Point(rand.Next(32), rand.Next(8) - 10);
 			}
 
-			int color;
-			public int Color
-			{
-				get => Tagged ? 0 : color;
-				set => color = value;
-			}
-
-			Point position;
-			public Point Position => position;
+			public int Color { get; set; }
+			public Point Position { get; set; }
+			public Point? Destination { get; set; }
 
 			bool tagged = true;
 			public bool Tagged
@@ -40,26 +32,8 @@ namespace Shelfinator.Creator.Patterns
 					if (tagged == value)
 						return;
 					tagged = value;
-					destination = null;
+					Destination = null;
 				}
-			}
-
-			List<Point> destination = null;
-
-			public void SetupWait(int time) => destination = Enumerable.Repeat(Position, time).ToList();
-
-			public void SetDestination(List<Point> destination) => this.destination = destination;
-
-			public Point NextPosition => destination.First();
-
-			public bool HasDestination => destination != null;
-
-			public void AcceptNext()
-			{
-				position = NextPosition;
-				destination.RemoveAt(0);
-				if (!destination.Any())
-					destination = null;
 			}
 		}
 
@@ -69,89 +43,82 @@ namespace Shelfinator.Creator.Patterns
 			bodyLayout = new Layout("Shelfinator.Creator.Patterns.Layout.Layout-Body.png");
 		}
 
-		int? GetLight(Point p) => p.Y < 0 ? headerLayout.TryGetPositionLight(new Point(p.X - HeaderOffsetX, p.Y - HeaderOffsetY)) : bodyLayout.TryGetPositionLight(p);
-
-		List<Point> CalcPoint(List<Point> curPoints, Point end, bool[] used)
+		int? GetLight(Point p, bool allowHeader = true)
 		{
-			var start = curPoints[curPoints.Count - 1];
-			if (start == end)
-				return curPoints;
-
-			var points = GetPoints(start, end);
-			foreach (var point in points)
-			{
-				var light = GetLight(point);
-				if ((!light.HasValue) || (used[light.Value]))
-					continue;
-
-				used[light.Value] = true;
-				curPoints.Add(point);
-				var result = CalcPoint(curPoints, end, used);
-				if (result != null)
-					return result;
-				used[light.Value] = false;
-				curPoints.RemoveAt(curPoints.Count - 1);
-			}
-
-			return null;
+			int? light = null;
+			if ((p.Y < 0) && (allowHeader))
+				light = light ?? headerLayout.TryGetPositionLight(new Point(p.X, p.Y + 10));
+			return light ?? bodyLayout.TryGetPositionLight(p);
 		}
 
-		List<Point> GetPoints(Point start, Point end)
+		Point NextLocation(Point location, Point dest, bool runAway = false)
 		{
-			var check = new List<Vector>();
-			var useX = Math.Abs(end.X - start.X) > Math.Abs(end.Y - start.Y);
-			for (var pass = 0; pass < 2; ++pass)
+			if ((location.Y < 0) && (dest.Y >= 0))
+				dest = new Point(((dest.X - 39) / 18 * 31).Round(), dest.Y);
+			else if ((location.Y >= 0) && (dest.Y < 0))
+				dest = new Point((dest.X / 31 * 18 + 39).Round(), dest.Y);
+
+			if ((location.Y == 0) && (dest.Y < 0))
 			{
-				Vector v1, v2;
-				if (useX)
-				{
-					v1 = new Vector(-1, 0);
-					v2 = new Vector(1, 0);
-				}
+				var p = new Point(((location.X - 39) / 18 * 31).Round(), -3);
+				if (GetLight(p, !runAway) != null)
+					return p;
+
+				return new Point(location.X + (dest.X < location.X ? -1 : 1), location.Y);
+			}
+			else if ((location.Y == -3) && (dest.Y >= 0))
+			{
+				var p = new Point(((location.X) / 31 * 18 + 39).Round(), 0);
+				if (GetLight(p, !runAway) != null)
+					return p;
+
+				return new Point(location.X + (dest.X < location.X ? -1 : 1), location.Y);
+			}
+			else if (Math.Abs(location.Y - dest.Y) >= Math.Abs(location.X - dest.X))
+			{
+				var next = new Point(location.X, location.Y + (location.Y <= dest.Y != runAway ? 1 : -1));
+				if (GetLight(next, !runAway) != null)
+					return next;
+				var x = location.X - location.X % 19 + 1;
+				var back = Math.Abs(x - dest.X) <= Math.Abs(x + 18 - dest.X);
+				next = new Point(location.X + (back != runAway ? -1 : 1), location.Y);
+				if (GetLight(next, !runAway) != null)
+					return next;
+				next = new Point(location.X + (back == runAway ? -1 : 1), location.Y);
+				if (GetLight(next, !runAway) != null)
+					return next;
+			}
+			else
+			{
+				var next = new Point(location.X + (location.X <= dest.X != runAway ? 1 : -1), location.Y);
+				if (GetLight(next, !runAway) != null)
+					return next;
+				bool back;
+				if (location.Y < 0)
+					back = dest.Y < location.Y;
 				else
 				{
-					v1 = new Vector(0, -1);
-					v2 = new Vector(0, 1);
+					var y = location.Y - location.Y % 19 + 1;
+					back = Math.Abs(y - dest.Y) <= Math.Abs(y + 18 - dest.Y);
 				}
 
-				var swap = ((useX) && (end.X > start.X)) || (!useX) && (end.Y > start.Y);
-				if (swap)
-				{
-					var val = v1;
-					v1 = v2;
-					v2 = val;
-				}
-
-				var index = check.Any() ? 1 : 0;
-				check.Insert(index, v2);
-				check.Insert(index, v1);
-
-				useX = !useX;
+				next = new Point(location.X, location.Y + (back != runAway ? -1 : 1));
+				if (GetLight(next, !runAway) != null)
+					return next;
+				next = new Point(location.X, location.Y + (back == runAway ? -1 : 1));
+				if (GetLight(next, !runAway) != null)
+					return next;
 			}
 
-			return check.Select(v => start + v).ToList();
+			throw new Exception("Cannot find next point");
 		}
 
-		List<Point> CalcPath(Point start, Point end)
+		void MoveHunters(List<Player> hunters, List<Player> players)
 		{
-			var points = CalcPoint(new List<Point> { start }, end, new bool[2440]);
-			if (points == null)
-				throw new Exception("Unable to calculate path");
-			points.RemoveAt(0);
-			return points;
-		}
-
-		void SetupHunterPaths(List<Player> hunters, List<Player> players)
-		{
-			hunters.ForEach(hunter => hunter.SetDestination(new List<Point> { hunter.Position }));
-
-			do
+			while (true)
 			{
 				hunters = hunters.Where(hunter => !hunter.Tagged).ToList();
 				players = players.Where(player => !player.Tagged).ToList();
-
-				if ((!hunters.Any()) || (!players.Any()))
-					break;
 
 				var ideas = hunters.SelectMany(hunter => players.Select(player => Tuple.Create(hunter, player, (hunter.Position - player.Position).LengthSquared))).OrderBy(idea => idea.Item3).ToList();
 				var tagged = ideas.Where(idea => idea.Item3 <= 1).Select(idea => idea.Item2).ToList();
@@ -162,17 +129,19 @@ namespace Shelfinator.Creator.Patterns
 				}
 
 				var busy = new HashSet<Player>();
-				foreach (var hunter in hunters)
+				foreach (var hunter in ideas.Select(idea => idea.Item1).Distinct())
 				{
 					var close = ideas.Where(idea => idea.Item1 == hunter).Select(idea => idea.Item2).ToList();
 					var closest = close.Where(player => !busy.Contains(player)).Concat(close).First();
 					busy.Add(closest);
-					hunter.SetDestination(CalcPath(hunter.Position, closest.Position));
+					hunter.Position = NextLocation(hunter.Position, closest.Position);
 				}
-			} while (false);
+
+				break;
+			}
 		}
 
-		void SetupPlayerPaths(List<Player> hunters, List<Player> players)
+		void MovePlayers(List<Player> hunters, List<Player> players)
 		{
 			foreach (var player in players)
 			{
@@ -181,39 +150,23 @@ namespace Shelfinator.Creator.Patterns
 					var closest = hunters.Where(hunter => (!hunter.Tagged)).OrderBy(hunter => (player.Position - hunter.Position).LengthSquared).FirstOrDefault();
 					if ((closest != null) && ((closest.Position - player.Position).Length < RunAwayDistance))
 					{
-						var point = GetPoints(player.Position, closest.Position).Where(p => (p.Y >= 0) && (GetLight(p).HasValue)).LastOrDefault();
-						if (point != null)
-							player.SetDestination(new List<Point> { point });
+						player.Position = NextLocation(player.Position, closest.Position, true);
+						player.Destination = null;
+						continue;
 					}
 				}
 
-				if (!player.HasDestination)
-				{
+				if (player.Destination == null)
 					while (true)
 					{
-						Point dest;
-
-						if (!player.Tagged)
-							dest = new Point(rand.Next(97), rand.Next(97));
-						else if (player.Position.Y >= 0)
-							dest = new Point(Math.Max(HeaderOffsetX, Math.Min(player.Position.X, 31 + HeaderOffsetX)), -1);
-						else
-							dest = new Point(rand.Next(32) + HeaderOffsetX, rand.Next(8) + HeaderOffsetY);
-
-						if (dest == player.Position)
-							continue;
-
-						if (GetLight(dest) == null)
-							continue;
-
-						var destination = CalcPath(player.Position, dest);
-						if (!destination.Any())
-							continue;
-
-						player.SetDestination(destination);
-						break;
+						player.Destination = player.Tagged ? new Point(rand.Next(32), rand.Next(8) - 10) : new Point(rand.Next(97), rand.Next(97));
+						if ((player.Destination.Value != player.Position) && (GetLight(player.Destination.Value) != null))
+							break;
 					}
-				}
+
+				player.Position = NextLocation(player.Position, player.Destination.Value);
+				if (player.Position == player.Destination.Value)
+					player.Destination = null;
 			}
 		}
 
@@ -229,18 +182,17 @@ namespace Shelfinator.Creator.Patterns
 		public int RenderGame(Pattern pattern, int numHunters, int numPlayers, int startTime)
 		{
 			const double Brightness = 1f / 16;
+			var colors = new List<int> { 0xff0000, 0x00ff00, 0xffff00, 0xff00ff, 0xff8000 };
 
 			const int TimeMultiplier = 5;
-			const int HunterLockup = 800;
+			const int HunterLockup = 1000;
 			const int PlayerLockup = 500;
 			const int EndingDelay = 500;
 
 			Setup();
 
-			var color = new LightColor(0, 6, new List<int> { 0xffffff, 0x0000ff, 0xffff00, 0xff8000, 0xff0000, 0x80ff00, 0x00ff00 }.Multiply(Brightness).ToList());
-
 			var hunters = Enumerable.Range(0, numHunters).Select(x => new Player()).ToList();
-			var players = Enumerable.Range(0, numPlayers).Select(x => new Player { Color = rand.Next(5) + 2 }).ToList();
+			var players = Enumerable.Range(0, numPlayers).Select(x => new Player { Color = colors[x % colors.Count] }).ToList();
 
 			var time = 0;
 			var stopTime = default(int?);
@@ -254,14 +206,12 @@ namespace Shelfinator.Creator.Patterns
 				var draw = false;
 				if (time % 4 == 0)
 				{
-					SetupHunterPaths(hunters, players);
-					hunters.ForEach(hunter => hunter.AcceptNext());
+					MoveHunters(hunters, players);
 					draw = true;
 				}
 				if (time % 6 == 0)
 				{
-					SetupPlayerPaths(hunters, players);
-					players.ForEach(player => player.AcceptNext());
+					MovePlayers(hunters, players);
 					draw = true;
 				}
 
@@ -271,8 +221,8 @@ namespace Shelfinator.Creator.Patterns
 						stopTime = stopTime ?? time + EndingDelay;
 
 					pattern.Clear(startTime + time);
-					players.ForEach(player => pattern.AddLight(GetLight(player.Position).Value, startTime + time, color, player.Tagged ? 1 : player.Color));
-					hunters.ForEach(hunter => pattern.AddLight(GetLight(hunter.Position).Value, startTime + time, color, 0));
+					players.ForEach(player => pattern.AddLight(GetLight(player.Position).Value, startTime + time, pattern.Absolute, Helpers.MultiplyColor(player.Tagged ? 0x0000ff : player.Color, Brightness)));
+					hunters.ForEach(hunter => pattern.AddLight(GetLight(hunter.Position).Value, startTime + time, pattern.Absolute, Helpers.MultiplyColor(0xffffff, Brightness / (hunter.Tagged ? 3 : 1))));
 				}
 
 				++time;
