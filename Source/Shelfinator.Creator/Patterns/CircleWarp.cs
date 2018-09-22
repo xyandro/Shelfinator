8c610d167394348312a3647030e36572cf429476
rs <spamnuker2000@yahoo.com>
2018-09-22 03:39:25 -0600

    Change SquareWarp.cs to CircleWarp.cs

--- Source/Shelfinator.Creator/Patterns/CircleWarp.cs
+++ Source/Shelfinator.Creator/Patterns/CircleWarp.cs
@@ -1,8 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Windows;
 
 namespace Shelfinator.Creator.Patterns
 {
-	class SquareWarp : IPattern
+	class CircleWarp : IPattern
 	{
 		public int PatternNumber => 38;
 
@@ -16,12 +18,15 @@ namespace Shelfinator.Creator.Patterns
 
 			var color = new LightColor(0, 5, Helpers.Rainbow6.Multiply(Brightness).ToList());
 
+			var center = new Point(48, 48);
+			var distances = layout.GetAllLights().Select(light => Tuple.Create(light, (layout.GetLightPosition(light) - center).Length)).ToList();
+
 			for (var square = 0; square < 18; ++square)
 			{
-				for (var x = 0; x <= 48; ++x)
+				for (var x = 0; x <= 66; ++x)
 				{
 					var time = square * 17 + x;
-					var lights = layout.GetPositionLights(48 - x, 48 - x, x * 2 + 1, x * 2 + 1).Except(layout.GetPositionLights(48 - x + 1, 48 - x + 1, x * 2 - 1, x * 2 - 1));
+					var lights = distances.Where(tuple => (tuple.Item2 >= x) && (tuple.Item2 < x + 2)).Select(tuple => tuple.Item1).ToList();
 					foreach (var light in lights)
 						pattern.AddLight(light, time, color, square % 6);
 					foreach (var light in lights)
@@ -31,7 +36,7 @@ namespace Shelfinator.Creator.Patterns
 
 			pattern.AddLightSequence(0, 102, 5000);
 			pattern.AddLightSequence(102, 204, 5000, 2);
-			pattern.AddLightSequence(204, 338, 6773);
+			pattern.AddLightSequence(204, 356, 7655);
 
 			return pattern;
 		}
