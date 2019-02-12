a6a445bb4751e79edfac5043d9f6b1057aa150e6
Randon Spackman <randon.spackman@gmail.com>
2019-02-12 15:53:21 -0700

    Add new for new patterns

---
+++ Source/Shelfinator.Creator/Songs/New.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Shelfinator.Creator.SongData;
+
+namespace Shelfinator.Creator.Songs
+{
+	class New : ISong
+	{
+		public int SongNumber => 9;
+
+		readonly Layout bodyLayout = new Layout("Shelfinator.Creator.Songs.Layout.Layout-Body.png");
+
+		Segment TimeWarp(out int time)
+		{
+			const int Repeat = 10;
+			var frame1 = Enumerable.Range(0, 3).SelectMany(y => Enumerable.Range(0, 3).SelectMany(x => bodyLayout.GetPositionLights(x * 38, y * 38, 21, 21))).ToList();
+			var frame2 = Enumerable.Range(0, 2).SelectMany(y => Enumerable.Range(0, 2).SelectMany(x => bodyLayout.GetPositionLights(x * 38 + 19, y * 38 + 19, 21, 21))).ToList();
+			var segment = new Segment();
+			time = 0;
+			var delay = 1 << Repeat;
+			for (var ctr = 0; ctr < Repeat; ++ctr)
+			{
+				segment.Clear(time);
+				foreach (var light in frame1)
+					segment.AddLight(light, time, 0x101010);
+				time += delay;
+
+				segment.Clear(time);
+				foreach (var light in frame2)
+					segment.AddLight(light, time, 0x101010);
+				time += delay;
+				delay >>= 1;
+			}
+			return segment;
+		}
+
+		public Song Render()
+		{
+			var song = new Song("new.ogg");
+
+			// TimeWarp (0)
+			var timeWarp = TimeWarp(out var timeWarpTime);
+			song.AddSegment(timeWarp, 0, timeWarpTime, 0, 10000);
+
+			return song;
+		}
+	}
+}
