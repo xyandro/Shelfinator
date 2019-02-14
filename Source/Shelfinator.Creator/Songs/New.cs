01511d7eadb99af85ea0696536e916e3a64c714c
Randon Spackman <randon.spackman@gmail.com>
2019-02-14 10:25:18 -0700

    Add wipe

--- Source/Shelfinator.Creator/Songs/New.cs
+++ Source/Shelfinator.Creator/Songs/New.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using Shelfinator.Creator.SongData;
 
 namespace Shelfinator.Creator.Songs
@@ -34,13 +37,48 @@ namespace Shelfinator.Creator.Songs
 			return segment;
 		}
 
+		Segment Wipe(out int time)
+		{
+			var funcs = new List<Tuple<Func<Point, int>, LightColor>>
+			{
+				new Tuple<Func<Point, int>, LightColor>(p => (((p - Helpers.Center).Length - 9) * 16.9830463869911).Round(), new LightColor(0, 1000, new List<int> { 0x100000, 0x010000 })),
+				new Tuple<Func<Point, int>, LightColor>(p => (p.X * 10.4166666666667).Round(), new LightColor(0, 1000, new List<int> { 0x001000, 0x000100 })),
+				new Tuple<Func<Point, int>, LightColor>(p => (p.Y * 10.4166666666667).Round(), new LightColor(0, 1000, new List<int> { 0x000010, 0x000001 })),
+				new Tuple<Func<Point, int>, LightColor>(p => ((p.X + p.Y) * 5.20833333333333).Round(), new LightColor(0, 1000, new List<int> { 0x100000, 0x001000 })),
+				new Tuple<Func<Point, int>, LightColor>(p => ((p.X - p.Y + 96) * 5.20833333333333).Round(), new LightColor(0, 1000, new List<int> { 0x001000, 0x000010 })),
+				new Tuple<Func<Point, int>, LightColor>(p => (1000 - ((p - Helpers.Center).Length - 9) * 16.9830463869911).Round(), new LightColor(0, 1000, new List<int> { 0x000010, 0x100000 })),
+				new Tuple<Func<Point, int>, LightColor>(p => (1000 - p.X * 10.4166666666667).Round(), new LightColor(0, 1000, new List<int> { 0x100010, 0x001010 })),
+				new Tuple<Func<Point, int>, LightColor>(p => (1000 - p.Y * 10.4166666666667).Round(), new LightColor(0, 1000, new List<int> { 0x101010, 0x000000, 0x101010 })),
+				new Tuple<Func<Point, int>, LightColor>(p => (1000 - (p.X + p.Y) * 5.20833333333333).Round(), new LightColor(0, 1000, new List<int> { 0x100000, 0x001000, 0x000010 })),
+				new Tuple<Func<Point, int>, LightColor>(p => (1000 - (p.X - p.Y + 96) * 5.20833333333333).Round(), new LightColor(0, 1000, Helpers.Rainbow6)),
+			};
+
+			var segment = new Segment();
+			time = 0;
+			foreach (var func in funcs)
+			{
+				foreach (var light in bodyLayout.GetAllLights())
+				{
+					var point = bodyLayout.GetLightPosition(light);
+					var delay = func.Item1(point);
+					segment.AddLight(light, time + delay, func.Item2, delay);
+				}
+				time += 2000;
+			}
+			return segment;
+		}
+
 		public override Song Render()
 		{
 			var song = new Song("new.ogg");
 
-			// TimeWarp (0)
-			var timeWarp = TimeWarp(out var timeWarpTime);
-			song.AddSegment(timeWarp, 0, timeWarpTime, 0, 10000);
+			//// TimeWarp (0)
+			//var timeWarp = TimeWarp(out var timeWarpTime);
+			//song.AddSegment(timeWarp, 0, timeWarpTime, 0, 10000);
+
+			// Wipe (0)
+			var wipe = Wipe(out var wipeTime);
+			song.AddSegment(wipe, 0, wipeTime, 0, wipeTime);
 
 			return song;
 		}
